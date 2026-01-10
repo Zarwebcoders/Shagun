1b0882847846459cfb9f0f2e938b05178439737c
b3ingsahil007 <chhipasahil163@gmail.com>
2026-01-10 13:56:37 +0530

    CORS Error

--- server/server.js
+++ server/server.js
@@ -13,7 +13,13 @@ connectDB();
 
 // CORS
 const corsOptions = {
-    origin: 'http://localhost:5000/api' || 'https://shagunbackend.vercel.app/api' || 'https://shagunbackend.vercel.app' || 'https://shagun-black.vercel.app',
+    origin: [
+        'http://localhost:5000',
+        'https://shagunbackend.vercel.app',
+        'https://shagun-black.vercel.app',
+        'http://localhost:3000',
+        'http://localhost:5173'
+    ],
     credentials: true,
     optionSuccessStatus: 200
 }
