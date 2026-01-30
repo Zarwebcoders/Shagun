cc09cea7cf86dbfc359f3a8baaf5cac47cac85ec
b3ingsahil007 <chhipasahil163@gmail.com>
2026-01-30 19:07:57 +0530

    Login Error Checking

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: import.meta.env.PROD ? 'https://shagunbackend.vercel.app/api' : '/api',
+    baseURL: import.meta.env.DEV ? '/api' : 'https://shagunbackend.vercel.app/api',
     headers: {
         'Content-Type': 'application/json',
     },
